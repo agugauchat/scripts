dfb0ed2b20ae6e70fbded4d0a1b0e406b40fac9a
Agu <agu.gauchat@gmail.com>
2017-06-22 23:26:48 -0300

    Add math_problem

---
+++ math_problem.py
@@ -0,0 +1,4 @@
+# La afirmación es la siguiente:
+"""Si uno dobla una hoja de papel de 0.1 mm de grosor
+y logra doblarla 50 veces, el grosor del resultado
+ocuparía la distancia de la Tierra al sol."""
